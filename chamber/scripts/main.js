e1211ae420ec8d61f281cb482c6dd0600426cf6c
Jordan1819 <jmanwaite@gmail.com>
2023-01-28 21:05:37 -0700

    W4

---
+++ chamber/scripts/main.js
@@ -0,0 +1,15 @@
+/* Author: Jordan Waite
+File: Javascript for chamber homepage*/
+
+const mainNav = document.querySelector('.navigation');
+const menuButton = document.querySelector('#menu');
+
+menuButton.addEventListener('click', () =>
+{mainNav.classList.toggle('responsive')}, false);
+
+const d = new Date();
+const fulldate = `Current Date: ${d}`;
+
+document.querySelector()
+
+
