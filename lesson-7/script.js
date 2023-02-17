69cf8fa65a570d6c242dddd141b79273b80e8e6b
Jordan1819 <jmanwaite@gmail.com>
2023-02-17 00:03:46 -0700

    Final Updates

--- lesson-7/script.js
+++ lesson-7/script.js
@@ -1,6 +1,8 @@
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
+const dataSrc = image.getAttribute("data-src")
+if (!dataSrc) return
+    image.setAttribute("src", dataSrc);
     image.onload = () => {
         image.removeAttribute("data-src");
     };
@@ -9,3 +11,21 @@ const loadImages = (image) => {
 imagesToLoad.forEach((img) => {
     loadImages(img);
 });
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
