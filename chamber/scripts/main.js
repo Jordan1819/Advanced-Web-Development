e584c2635a963de5cb28494489efc8e781833c16
Jordan1819 <jmanwaite@gmail.com>
2023-02-20 19:30:08 -0700

    2.2

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -40,3 +40,36 @@ const currentDay = date.getDay();
 if (currentDay == 1 || currentDay == 2) {
     promotion.style.display = 'block';
 }
+
+/* Lazy loading of images here */
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+const dataSrc = image.getAttribute("data-src")
+if (!dataSrc) return
+    image.setAttribute("src", dataSrc);
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
