12842e725ce02e6ff2c78cc843fa7e3142ad5171
Jordan1819 <jmanwaite@gmail.com>
2023-02-23 05:55:08 -0700

    mobile discover optimized

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -33,6 +33,7 @@ document.querySelector('.headerDate').textContent = head;
 
 /* The code below is for displaying a promotional
 message on Mondays and Tuesdays */
+
 const promotion = document.querySelector('.promotion');
 const date = new Date();
 const currentDay = date.getDay();
@@ -42,6 +43,7 @@ if (currentDay == 1 || currentDay == 2) {
 }
 
 /* Lazy loading of images here */
+
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
 const dataSrc = image.getAttribute("data-src")
@@ -73,3 +75,24 @@ if ("IntersectionObserver" in window) {
       loadImages(img);
     });
   }
+
+  /* Section for calculating number of days since last visit */
+
+  let diffDays = 0; 
+    
+  //get the value from localStorage
+  const startDate = localStorage.getItem("lastDate");
+
+  //if it is not null calculate the difference
+  if(startDate != null){
+    const difference = (new Date().getTime() - startDate) / 1000 / 60 / 60 / 24; //milliseconds to the day
+    diffDays = Math.floor(difference);
+    document.querySelector(".timeBetween").innerText = "Days since your last visit: " + diffDays;
+  } else {
+    document.querySelector(".timeBetween").innerText = "This is your first time here! Welcome!"
+  }
+
+//write current date to the localStorage again
+localStorage.setItem("lastDate", (new Date()).getTime());
+ 
+
