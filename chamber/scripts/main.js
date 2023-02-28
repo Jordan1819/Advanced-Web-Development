ff8dc823597fe04797f803bd75dc6a7484a7c9d5
Jordan1819 <jmanwaite@gmail.com>
2023-02-28 07:22:27 -0700

    join updates

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -12,7 +12,7 @@ menuButton.addEventListener('click', () =>
 /* The code below is for inserting the current date
 into the header and footer */
 
-const d = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
+var d = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
 const yearDate = new Date();
 const year = yearDate.getFullYear();
 
@@ -75,7 +75,7 @@ if ("IntersectionObserver" in window) {
       loadImages(img);
     });
   }
-
+  /* Discove Page */
   /* Section for calculating number of days since last visit */
 
   let diffDays = 0; 
@@ -94,5 +94,51 @@ if ("IntersectionObserver" in window) {
 
 //write current date to the localStorage again
 localStorage.setItem("lastDate", (new Date()).getTime());
- 
+
+
+// Join Page
+
+function validateTitleInput(inputValue) {
+  // Regular expression to check for valid title format
+  const titleRegex = /^[A-Za-z\s-]{7,}*$/;
+
+  // Check if the input value matches the regex pattern
+  if (titleRegex.test(inputValue)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateForm() {
+  const titleInput = document.getElementById("title");
+  const title = titleInput.value;
+  if (!validateTitleInput(title)) {
+    titleInput.setCustomValidity("Please enter a valid title with alpha characters, hyphens, and spaces and a minimum of seven characters.");
+    return false;
+  }
+  // If the title is valid, allow the form to submit
+  return true;
+}
+
+// Code to add current date to hidden form
+window.onload = function() {
+  storeCurrentDate();
+}
+
+function storeCurrentDate() {
+  // Create a new Date object representing the current date and time
+  var currentDate = new Date();
+  
+  // Get the hidden input element with an id of "dateAccessed"
+  const dateInput = document.getElementById("dateAccessed");
+
+  // Set the value of the input element to the current date
+  dateInput.value = currentDate.toISOString().slice(0, 10);
+
+  console.log(currentDate);
+}
+
+
+
 
