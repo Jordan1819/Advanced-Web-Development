2ab433e9f786ae9bc2a20506a40dbc8d8dc06c44
Jordan1819 <jmanwaite@gmail.com>
2023-02-28 21:51:59 -0700

    CSS

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -76,22 +76,3 @@ if ("IntersectionObserver" in window) {
       loadImages(img);
     });
   }
-  /* Discover Page */
-  /* Section for calculating number of days since last visit */
-
-  let diffDays = 0; 
-    
-  //get the value from localStorage
-  const startDate = localStorage.getItem("lastDate");
-
-  //if it is not null calculate the difference
-  if(startDate != null){
-    const difference = (new Date().getTime() - startDate) / 1000 / 60 / 60 / 24; //milliseconds to the day
-    diffDays = Math.floor(difference);
-    document.querySelector(".timeBetween").innerText = "Days since your last visit: " + diffDays;
-  } else {
-    document.querySelector(".timeBetween").innerText = "This is your first time here! Welcome!"
-  }
-
-//write current date to the localStorage again
-localStorage.setItem("lastDate", (new Date()).getTime());
